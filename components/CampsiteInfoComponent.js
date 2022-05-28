250601455ffb5b4783d4aeaa362ad0b65a8b4b56
Kalle Miller <kallegmiller@gmail.com>
2022-05-28 11:22:12 -0700

    Week 3 Workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -27,11 +27,14 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
-            view.current.rubberBand(1000)
-            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+            view.current
+                .rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
@@ -53,9 +56,11 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
-        }
+        },
     });
     
     if (campsite) {
